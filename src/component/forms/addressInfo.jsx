bba2edd1c266c5b1ccbaea092dc0ad9f6d35f33c
MehediHasan06 <mehedihasanmasud06@gmail.com>
2021-01-17 18:24:46 +0600

    address form is added

--- src/component/forms/addressInfo.jsx
+++ src/component/forms/addressInfo.jsx
@@ -1,85 +1,253 @@
-const Address = () => {
-    return ( 
-        <>
-            <form>
-                <h1 className="text-center">Address Information</h1>
-                <h3>Permanent Address</h3>
+import {Button} from "react-bootstrap";
+import React,{Component} from "react";
+import {connect} from "react-redux";
+import axios from 'axios'
 
-                <label>District</label>
-                <input
-                    className="form-control"
-                    type="text"
-                    name="district"
-                />
-                
-                <label>Upazilla /PS </label>
-                <input
-                    className="form-control"
-                    type="text"
-                    name="upazilla"                
-                />
+class AddressInfo extends Component{
+    state = {
+        districts : [],
+        upazillas : [],
+        postOffices : [],
 
-                <label>Post Office</label>
-                <input
-                    className="form-control"
-                    type="text"
-                    name="post_office"
-                />
+        present_district : "",
+        present_upazilla : "",
+        present_postOffice : "",
+        present_village : "",
+        present_road : "",  
 
-                <label>Village /House</label>
-                <input
-                    className="form-control"
-                    type="text"
-                    name="road"
-                />
+        permanent_district : "",
+        permanent_upazilla : "",
+        permanent_postOffice : "",
+        permanent_village : "",
+        permanent_road : "",
+    }
 
-                <label>Road /Block /Sector</label>
-                <input
-                    className="form-control"
-                    type="text"
-                    name="house"
-                />
+    componentDidMount() {
+        this.fetchDistrict();
+        this.fetchUpazilla();
+        this.fetchPostOffice();
 
-                <h3>Present Address</h3>
+    }
 
-                <label>District</label>
-                <input
-                    className="form-control"
-                    type="text"
-                    name="district"
-                />
+    fetchDistrict = async () => {
+        await axios.get(`/api/dropdownApi/getDistrict`)
+        .then(res => {
+            this.setState({
+                districts : res.data.data
+            })
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    };
 
-                <label>Upazilla /PS </label>
-                <input
-                    className="form-control"
-                    type="text"
-                    name="upazilla"                
-                />
+    fetchUpazilla = async () => {
+        await axios.get(`/api/dropdownApi/getUpazilla`)
+        .then(res => {
+            this.setState({
+                upazillas : res.data.data
+            })
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    };
 
-                <label>Post Office</label>
-                <input
-                    className="form-control"
-                    type="text"
-                    name="post_office"
-                />
+    fetchPostOffice = async () => {
+        await axios.get(`/api/dropdownApi/getPostOffice`)
+        .then(res => {
+            this.setState({
+                postOffices : res.data.data
+            })
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    };
 
-                <label>Village /House</label>
-                <input
-                    className="form-control"
-                    type="text"
-                    name="road"
-                />
+    onChangeHandler = change => {
+        this.setState({
+            [change.target.name] : change.target.value
+        })
+    };
 
-                <label>Road /Block /Sector</label>
-                <input
-                    className="form-control"
-                    type="text"
-                    name="house"
-                />
+    onSubmit = submit => {
+        submit.preventDefault();
+        const { 
+            present_district,
+            present_upazilla,
+            present_postOffice,
+            present_village,
+            present_road,
+            permanent_district,
+            permanent_upazilla,
+            permanent_postOffice,
+            permanent_village,
+            permanent_road
+        } = this.state;
 
-            </form>
-        </>
-    );
+        const addressInfo = {
+            employee: this.props.auth.user.id,
+            present_district,
+            present_upazilla,
+            present_postOffice,
+            present_village,
+            present_road,
+            permanent_district,
+            permanent_upazilla,
+            permanent_postOffice,
+            permanent_village,
+            permanent_road
+        }
+
+        axios.post('/api/dbForms/addAddressInfo', addressInfo)
+            .then( res => {
+                this.setState({
+                    addressInfos : res.data
+                })
+                alert('Successfully Added !');
+            })
+            .catch( error => {
+                console.log(error);
+            })
+    } 
+    
+    render() {
+        return ( 
+            <>
+                <div className="container">
+                    <form onSubmit={this.onSubmit}>
+                    <h1 className="text-center">Address Information</h1>
+                    <h3>Present Address</h3>
+    
+                    <div className="form-group">
+                        <label htmlFor="exampleFormControlSelect1">District</label>
+                        <select className="form-control" id="exampleFormControlSelect1" name="present_district" onChange={this.onChangeHandler}>
+                            <option>Choose Your District</option>
+                            {
+                                this.state.districts.length > 0 && this.state.districts.map( (district,key) => (
+                                    <option key={key} value={district._id}>{district.districtName}</option>
+                                ) )
+
+                            }
+                        </select>
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="exampleFormControlSelect2">Upazilla /PS</label>
+                        <select className="form-control" id="exampleFormControlSelect2" name="present_upazilla" onChange={this.onChangeHandler}>
+                            <option>Choose Your Upazilla</option>
+                            {
+                                this.state.upazillas.length > 0 && this.state.upazillas.map( (upazilla,key) => (
+                                    <option key={key} value={upazilla._id}>{upazilla.upazillaName}</option>
+                                ) )
+
+                            }
+                        </select>
+                    </div>
+                    
+                    <div className="form-group">
+                        <label htmlFor="exampleFormControlSelect3">Post Office</label>
+                        <select className="form-control" id="exampleFormControlSelect3" name="present_postOffice" onChange={this.onChangeHandler}>
+                            <option>Choose Your Post Office</option>
+                            {
+                                this.state.postOffices.length > 0 && this.state.postOffices.map( (postOffice,key) => (
+                                    <option key={key} value={postOffice._id}>{postOffice.postOfficeName}</option>
+                                ) )
+
+                            }
+                        </select>
+                    </div>                        
+    
+                    <label>Village /House</label>
+                    <input
+                        className="form-control"
+                        type="text"
+                        name="present_village"
+                        onChange={this.onChangeHandler}
+                    />
+    
+                    <label>Road /Block /Sector</label>
+                    <input
+                        className="form-control"
+                        type="text"
+                        name="present_road"
+                        onChange={this.onChangeHandler}
+                    />
+    
+                    <h3>Permanenet Address</h3>
+    
+                    <div className="form-group">
+                        <label htmlFor="exampleFormControlSelect1">District</label>
+                        <select className="form-control" id="exampleFormControlSelect1" name="permanent_district" onChange={this.onChangeHandler}>
+                            <option>Choose Your District</option>
+                            {
+                                this.state.districts.length > 0 && this.state.districts.map( (district,key) => (
+                                    <option key={key} value={district._id}>{district.districtName}</option>
+                                ) )
+
+                            }
+                        </select>
+                    </div>
+                    
+                    <div className="form-group">
+                        <label htmlFor="exampleFormControlSelect2">Upazilla /PS</label>
+                        <select className="form-control" id="exampleFormControlSelect2" name="permanent_upazilla" onChange={this.onChangeHandler}>
+                            <option>Choose Your Upazilla</option>
+                            {
+                                this.state.upazillas.length > 0 && this.state.upazillas.map( (upazilla,key) => (
+                                    <option key={key} value={upazilla._id}>{upazilla.upazillaName}</option>
+                                ) )
+
+                            }
+                        </select>
+                    </div>
+    
+                    <div className="form-group">
+                        <label htmlFor="exampleFormControlSelect3">Post Office</label>
+                        <select className="form-control" id="exampleFormControlSelect3" name="permanent_postOffice" onChange={this.onChangeHandler}>
+                            <option>Choose Your Post Office</option>
+                            {
+                                this.state.postOffices.length > 0 && this.state.postOffices.map( (postOffice,key) => (
+                                    <option key={key} value={postOffice._id}>{postOffice.postOfficeName}</option>
+                                ) )
+
+                            }
+                        </select>
+                    </div>
+    
+                    <label>Village /House</label>
+                    <input
+                        className="form-control"
+                        type="text"
+                        name="permanent_village"
+                        onChange={this.onChangeHandler}
+                    />
+    
+                    <label>Road /Block /Sector</label>
+                    <input
+                        className="form-control"
+                        type="text"
+                        name="permanent_road"
+                        onChange={this.onChangeHandler}
+                    />
+
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+    
+                </form>
+                </div>
+            </>
+        );
+    }
+
 }
- 
-export default Address;
+
+const mapStateToProps = state => ({
+    auth: state.auth,
+    errors: state.errors
+})
+
+export default connect(mapStateToProps,null)(AddressInfo);    
+
